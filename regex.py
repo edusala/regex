ce3ac1902928191a5d4e5ace4182528050ae2f58
Sallai Andras <termih@gmail.com>
2020-07-25 19:00:01 +0200

    ip címre működik

--- regex.py
+++ regex.py
@@ -3,7 +3,7 @@ import re
 print("Regex teszt")
 ipcim = input("Ip cím: ")
 
-egyezik = re.match(r'^[0-9]{1,3}$', ipcim)
+egyezik = re.match(r'^([0-9]{1,3}\.){3}[0-9]{1,3}$', ipcim)
 
 if egyezik:
 	print("Egyezik")
